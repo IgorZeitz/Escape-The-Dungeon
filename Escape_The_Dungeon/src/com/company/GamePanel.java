bce2df79b660de6d1dcb8adf3548eae12d755de6
IgorZeitz <110420524+IgorZeitz@users.noreply.github.com>
2022-10-25 17:16:19 +0200

    Add files via upload
    Basic player movement and functional game loop

--- Escape_The_Dungeon/src/com/company/GamePanel.java
+++ Escape_The_Dungeon/src/com/company/GamePanel.java
@@ -37,12 +37,6 @@ public class GamePanel extends JPanel implements Runnable{
 
         }
     }
-    //
-
-    //game loop
-
-
-
 
 
 //fps operation in the background **********************************
@@ -53,24 +47,54 @@ public class GamePanel extends JPanel implements Runnable{
         frameRate.start();
     }
 
+//------------ loop ------------------------------------------------------------
+    double delay=1000000000/60; //60 frames (in 1 sec - 1000ms- 1000 000 000ns)
+    long time = System.nanoTime();
+
+    double delta=0;
+
+
     @Override
     public void run() {
         while (frameRate!=null){
 
-         //   System.out.println("liczy?");
-            //counting
-
             updateGame();
 
-
             repaint();
+
+            time+=delay;
+                try {
+                    long time2=System.nanoTime();
+                    delta=time-time2;
+                    delta=delta/1000000;
+                    if(delta>=0) {
+                        Thread.sleep((long) delta);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
         }
     }
+
+//Player positioning - moving (by changing x&y)
+    int playerPositionX = 100;
+    int playerPositionY = 100;
+//------------------------------------------------------
 
     public void updateGame() {
 
         if(keyControls.up == true){
-            //przesunięcie pozycji?
+            playerPositionY-=3; //moving up by 3pix
+        }
+        if(keyControls.down==true){
+            playerPositionY+=3;
+        }
+        if(keyControls.left==true){
+            playerPositionX-=3;
+        }
+        if(keyControls.right==true){
+            playerPositionX+=3;
         }
     }
     public void paintComponent(Graphics g){
@@ -78,7 +102,7 @@ public class GamePanel extends JPanel implements Runnable{
         Graphics2D g2 = (Graphics2D)g;      //Extenciton of graphis specified for 2d
 
         g2.setColor(Color.red);
-        g2.fillRect(100, 100, screenSize, screenSize);      //main character position + size of main character
+        g2.fillRect(playerPositionX, playerPositionY, screenSize, screenSize);      //main character position + size of main character
     }
 }
 //***********************************************
